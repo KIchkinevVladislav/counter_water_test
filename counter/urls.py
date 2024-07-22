2950f18bf1a921f085d81b78fba88a61a6fe91cd
Vladislav <v.kichkinev@microline.ru>
2024-07-22 14:53:14 +0300

    style(counter/urls.py): correct typo

--- counter/urls.py
+++ counter/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('apartment-building/<int:pk>/', ApartmentBuildingDetailView.as_view(), name='apartment_building_detail'),
     path('create/apartment-building/', ApartmentBuildingCreateView.as_view(), name='apartment_building_create'),
     path('create/flat/', FlatCreateView.as_view(), name='flat-create'),
-    path('create/water_counter/', WaterCounterCreateView.as_view(), name='water_counter_create'),
-    path('add-meter-reading', AddMeterReadingView.as_view(), name='add_meter_reading'),
+    path('create/water-counter/', WaterCounterCreateView.as_view(), name='water_counter_create'),
+    path('add-meter-reading/', AddMeterReadingView.as_view(), name='add_meter_reading'),
     path('calculate-payment', CalculatePaymentView.as_view(), name='calculate_payment'),
     path('calculate-progress/<int:apartment_building_id>/', CalculationProgressView.as_view(), name='calculate_progress'),
 ]
